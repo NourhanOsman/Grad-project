afc9d96c819c86c4f7317060b8fcaa3600ae92f2
NourhanOsman <153376227+NourhanOsman@users.noreply.github.com>
2024-03-13 02:30:04 +0200

    Create String.h

---
+++ String Library/String.h
@@ -0,0 +1,9 @@
+#ifndef STRING_H_INCLUDED
+#define STRING_H_INCLUDED
+
+ int strlen(const char *str);
+ short int strcompare(const char* original, const char* desired, int size);
+ int strToInt(const char* str);
+ void strDelete(char* str, int size);
+
+#endif
